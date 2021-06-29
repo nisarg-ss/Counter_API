a6c069a5ad52716eb404b163236d96449ca5eabc
Nisarg Soni <nisarg.soni@smartsensesolutions.com>
2021-06-29 18:53:34 +0530

    update error handling

--- main.py
+++ main.py
@@ -6,45 +6,60 @@ dictionary={}
 
 @app.route('/<id>',methods=['GET'])
 def get_counter(id):
-    res=dictionary.get(id,None)
-    if res==None:
-        return {"Message":"Not present in System"}
-    print(dictionary)
-    return {"Counter":res}
+    try:
+        res=dictionary.get(id,None)
+        if res==None:
+            return {"Message":"Not present in System"}
+        print(dictionary)
+        return {"Counter":res}
+    except Exception as e:
+        return {'error':str(e)}
 
 @app.route('/add/<id>',methods=["POST"])
 def add_Counter(id):
-    dictionary[id]=0
-    print(dictionary)
-    return {"Message":"Data added to dictionary"}
+    try:
+        dictionary[id]=0
+        print(dictionary)
+        return {"Message":"Data added to dictionary"}
+    except Exception as e:
+        return {'error':str(e)}
 
 @app.route('/<id>/increment',methods=["POST"])
 def increment_Counter(id):
-    res=dictionary.get(id,None)
-    if res==None:
+    try:
+        res=dictionary.get(id,None)
+        if res==None:
+            print(dictionary)
+            return {"Message":"Not present in System"}
+        dictionary[id]=res+1
         print(dictionary)
-        return {"Message":"Not present in System"}
-    dictionary[id]=res+1
-    print(dictionary)
-    return {"Message":"Increment successfully !"}
+        return {"Message":"Increment successfully !"}
+    except Exception as e:
+        return {'error':str(e)}
 
 @app.route('/<id>/decrement',methods=['POST'])
 def decrement_count(id):
-    res=dictionary.get(id,None)
-    if res==None:
-        return {"Message":"Not present in System"}
-    dictionary[id]=res-1
-    print(dictionary)
-    return {"Message":"decrement successfully !"}
+    try:
+        res=dictionary.get(id,None)
+        if res==None:
+            return {"Message":"Not present in System"}
+        dictionary[id]=res-1
+        print(dictionary)
+        return {"Message":"decrement successfully !"}
+    except Exception as e:
+        return {'error':str(e)}
 
 @app.route('/<id>/delete',methods=['DELETE'])
 def del_Counter(id):
-    res=dictionary.get(id,None)
-    if res==None:
-        return {"Message":"Not present in System"}
-    del dictionary[id]
-    print(dictionary)
-    return {"Message":"counter deleted successfully !"}
+    try:
+        res=dictionary.get(id,None)
+        if res==None:
+            return {"Message":"Not present in System"}
+        del dictionary[id]
+        print(dictionary)
+        return {"Message":"counter deleted successfully !"}
+    except Exception as e:
+        return {'error':str(e)}
 
 if __name__=="__main__":
     app.run(debug=True)
--- main_withredis.py
+++ main_withredis.py
@@ -7,42 +7,57 @@ redis_server=redis.Redis('localhost',decode_responses=True)
 
 @app.route('/<id>',methods=['GET'])
 def get_counter(id):
-    res=(redis_server.hget('counters',id))
-    if res==None:
-        return {"Message":"Not present in System"}
-    return {"Counter":int(res)}
+    try:
+        res=(redis_server.hget('counters',id))
+        if res==None:
+            return {"Message":"Not present in System"}
+        return {"Counter":int(res)}
+    except Exception as e:
+        return {'error':str(e)}
 
 @app.route('/add/<id>',methods=["POST"])
 def add_Counter(id):
-    res=(redis_server.hget('counters',id))
-    if res!=None:
-        return {"Message":"the data with this id is already there !"}
-    redis_server.hset('counters',id,0)
-    return {"Message":"Data added to dictionary"}
+    try:
+        res=(redis_server.hget('counters',id))
+        if res!=None:
+            return {"Message":"the data with this id is already there !"}
+        redis_server.hset('counters',id,0)
+        return {"Message":"Data added to dictionary"}
+    except Exception as e:
+        return {'error':str(e)}
 
 @app.route('/<id>/increment',methods=["POST"])
 def increment_Counter(id):
-    res=redis_server.hget('counters',id)
-    if res==None:
-        return {"Message":"Not present in System"}
-    redis_server.hset('counters',id,int(res)+1)
-    return {"Message":"Increment successfully !"}
+    try:
+        res=redis_server.hget('counters',id)
+        if res==None:
+            return {"Message":"Not present in System"}
+        redis_server.hset('counters',id,int(res)+1)
+        return {"Message":"Increment successfully !"}
+    except Exception as e:
+        return {'error':str(e)}
 
 @app.route('/<id>/decrement',methods=['POST'])
 def decrement_count(id):
-    res=int(redis_server.hget('counters',id))
-    if res==None:
-        return {"Message":"Not present in System"}
-    redis_server.hset('counters',id,res-1)
-    return {"Message":"decrement successfully !"}
+    try:
+        res=int(redis_server.hget('counters',id))
+        if res==None:
+            return {"Message":"Not present in System"}
+        redis_server.hset('counters',id,res-1)
+        return {"Message":"decrement successfully !"}
+    except Exception as e:
+        return {'error':str(e)}
 
 @app.route('/<id>/delete',methods=['DELETE'])
 def del_Counter(id):
-    res=int(redis_server.hget('counters',id))
-    if res==None:
-        return {"Message":"Not present in System"}
-    redis_server.hdel('counters',id)
-    return {"Message":"counter deleted successfully !"}
+    try:
+        res=int(redis_server.hget('counters',id))
+        if res==None:
+            return {"Message":"Not present in System"}
+        redis_server.hdel('counters',id)
+        return {"Message":"counter deleted successfully !"}
+    except Exception as e:
+        return {'error':str(e)}
 
 if __name__=="__main__":
     app.run(debug=True)
